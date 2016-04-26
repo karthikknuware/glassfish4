8cdbbe8eee1249f804b8ac43f5bb628b64642641
karthik <karthik.koppuravuri@nuware.com>
2016-04-26 14:14:56 +0530

    second push

--- glass/recipes/default.rb
+++ glass/recipes/default.rb
@@ -7,7 +7,19 @@
 # All rights reserved - Do Not Redistribute
 #
 
-apt-get install python-software-properties
-add-apt-repository ppa:webupd8team/java
-apt-get update
-apt-get install oracle-java8-installer
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+bash "insert_line" do
+  user "root"
+  code <<-EOS
+  echo "JAVA_HOME="/usr/lib/jvm/java-8-oracle"" >> /etc/environment
+  EOS
+  not_if "grep -q JAVA_HOME= "/usr/lib/jvm/java-8-oracle" /etc/environment"
+end
+source /etc/environment
+apt-get install unzip
+wget download.java.net/glassfish/4.0/release/glassfish-4.0.zip
+unzip glassfish-4.0.zip -d /opt
+
