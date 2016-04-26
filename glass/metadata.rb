9c4264b96b457505ca5ced4905aeee919b5c1000
karthik <karthik.koppuravuri@nuware.com>
2016-04-26 14:01:15 +0530

    first change

---
+++ glass/metadata.rb
@@ -0,0 +1,7 @@
+name             'glass'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures glass'
+long_description 'Installs/Configures glass'
+version          '0.1.0'
---
+++ glass/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: glass
+# Recipe:: default
+#
+# Copyright (C) 2016 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+apt-get install python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer
