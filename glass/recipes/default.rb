e86df6279fbb31c1ad0926e45aa9c1811e646638
karthikknuware <karthik.koppuravuri@nuware.com>
2016-04-26 15:33:38 +0530

    done

--- glass/recipes/default.rb
+++ glass/recipes/default.rb
@@ -11,8 +11,10 @@ bash 'install-glassfish' do
 sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
  EOH
+end
+package 'oracle-java8-installer' do
+  action :install
 end
 bash "insert_line" do
   user "root"
