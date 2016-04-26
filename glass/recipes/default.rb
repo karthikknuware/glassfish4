3be595b7e030aef54c2e800403d6592c0e73524d
karthikknuware <karthik.koppuravuri@nuware.com>
2016-04-26 19:26:34 +0530

    Update default.rb

--- glass/recipes/default.rb
+++ glass/recipes/default.rb
@@ -11,10 +11,9 @@ bash 'install-glassfish' do
 sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo wget --no-cookies --no-check-certificate --header “Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie” “https://download.oracle.com/otn-pub/java/jdk/8u51-b16/jdk-8u51-linux-x64.rpm"
-sudo rpm -ivh jdk-8u51-linux-x64.rpm
  EOH
 end
+
 bash "insert_line" do
   user "root"
   code <<-EOS
